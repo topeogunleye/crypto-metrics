03529cc5639d3c030ad7e3ddd49736e0b36d7108
Temitope Ogunleye <topeogunleye1@gmail.com>
2022-02-07 16:06:20 +0100

    Add App Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core';
+import Details from './components/pages/Details';
+import Home from './components/pages/Home';
 import './App.css';
+import Header from './components/Header';
 
 function App() {
+  const useStyles = makeStyles(() => ({
+    App: {
+      backgroundColor: '#3f51b5',
+      color: 'white',
+      minHeight: '100vh',
+    },
+  }));
+
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      
-    </div>
+    <BrowserRouter>
+      <div className={classes.App}>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="details/*" element={<Details />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
