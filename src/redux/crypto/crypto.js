5c26c4fce6f4faa6cb44d3005d10754ac1461997
Temitope Ogunleye <topeogunleye1@gmail.com>
2022-02-08 21:51:18 +0100

    Add loadfilteredCryptos and singlecrptodetails

--- src/redux/crypto/crypto.js
+++ src/redux/crypto/crypto.js
@@ -1,9 +1,21 @@
 import axios from 'axios';
 
 const LOAD_CRYPTOS = 'LOAD_CRYPTOS';
+const LOAD_FILTERED_CRYPTOS = 'LOAD_FILTERED_CRYPTOS';
+const SINGLE_CRYPTO_DETAILS = 'SINGLE_CRYPTO_DETAILS';
 
 export const loadCryptos = (payload) => ({
   type: LOAD_CRYPTOS,
+  payload,
+});
+
+export const loadFilteredCryptos = (payload) => ({
+  type: LOAD_FILTERED_CRYPTOS,
+  payload,
+});
+
+export const singleCryptoDetails = (payload) => ({
+  type: SINGLE_CRYPTO_DETAILS,
   payload,
 });
 
@@ -25,13 +37,55 @@ export const fetchCryptos = (currency) => (dispatch) => {
     .catch(() => {});
 };
 
+export const filterCryptos = (cryptos, search) => (dispatch) => {
+  console.log(search);
+  if (!search) return cryptos;
+  const filteredCryptos = cryptos.filter((crypto) => crypto.name.toLowerCase().includes(search.toLowerCase()));
+
+  dispatch(loadCryptos(filteredCryptos));
+};
+
+export const fetchCryptoDetails = (id) => (dispatch) => {
+  console.log(id);
+  axios
+    .get(SingleCoin(id))
+    .then((response) => {
+      console.log(response.data);
+      const crypto = response.data;
+      dispatch(singleCryptoDetails(crypto));
+    })
+    .catch(() => {});
+};
+
 const initialState = {
   cryptos: [],
 };
 
-const reducer = (state = initialState, action) => {
+const initCryptoDetailsState = {
+  crypto: {},
+};
+
+export const cryptoDetailsReducer = (
+  state = initCryptoDetailsState,
+  action,
+) => {
+  switch (action.type) {
+    case SINGLE_CRYPTO_DETAILS:
+      return {
+        crypto: action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
+export const cryptosReducer = (state = initialState, action) => {
   switch (action.type) {
     case LOAD_CRYPTOS:
+      return {
+        cryptos: action.payload,
+      };
+    case LOAD_FILTERED_CRYPTOS:
       return {
         cryptos: action.payload,
       };
@@ -39,5 +93,3 @@ const reducer = (state = initialState, action) => {
       return state;
   }
 };
-
-export default reducer;
