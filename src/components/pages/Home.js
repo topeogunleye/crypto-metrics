ca404d9c387b03c3e29c50e0f410faf0a025fd75
Temitope Ogunleye <topeogunleye1@gmail.com>
2022-02-08 05:05:22 +0100

    Remove Material UI

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,7 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector } from 'react-redux';
 
 function Home() {
-  return <div>Home Page</div>;
+  const cryptos = useSelector((state) => state.cryptosReducer.cryptos);
+
+  useEffect(() => {
+    console.log(cryptos);
+  }, [cryptos]);
+
+  const cryptoDiv = cryptos.map((crypto) => (
+    <div key={crypto.id} cursor="pointer">
+      <h1>{crypto.name}</h1>
+      <img src={crypto.image} alt={crypto.name} />
+      <p>{crypto.description}</p>
+    </div>
+  ));
+
+  return <div> {cryptoDiv} </div>;
 }
 
 export default Home;
