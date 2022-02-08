6cd42123fc70b0c0bba79734e0b0fd89b22668fa
Temitope Ogunleye <topeogunleye1@gmail.com>
2022-02-08 05:05:01 +0100

    Add store provider

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import App from './App';
 import CryptoContext from './CryptoContext';
+import store from './redux/configureStore';
 
 ReactDOM.render(
   <React.StrictMode>
-    <CryptoContext>
-      <App />
-    </CryptoContext>
+    <Provider store={store}>
+      <CryptoContext>
+        <App />
+      </CryptoContext>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
