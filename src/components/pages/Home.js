ce36e4212c51673710c3e5de7c7fadf2f4aeeb99
Temitope Ogunleye <topeogunleye1@gmail.com>
2022-02-08 21:50:37 +0100

    Add JSX for Home page

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,22 +1,42 @@
-import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
+import './Meal.css';
 
 function Home() {
   const cryptos = useSelector((state) => state.cryptosReducer.cryptos);
 
-  useEffect(() => {
-    console.log(cryptos);
-  }, [cryptos]);
-
   const cryptoDiv = cryptos.map((crypto) => (
-    <div key={crypto.id} cursor="pointer">
-      <h1>{crypto.name}</h1>
-      <img src={crypto.image} alt={crypto.name} />
-      <p>{crypto.description}</p>
+
+    <div
+      key={crypto.id}
+      className="meal hover:shadow-lg transition-all duration-1000 ease-out"
+    >
+      <Link to={`/Details/${crypto.id}`}>
+        <h1>{crypto.name}</h1>
+        <img
+          src={crypto.image}
+          alt={crypto.name}
+          className="h-40 sm:h-40 w-full object-cover hover:opacity-75"
+        />
+        <p>
+          ₦
+          {crypto.current_price}
+        </p>
+      </Link>
     </div>
   ));
 
-  return <div> {cryptoDiv} </div>;
+  return (
+    <div className="bg-gray-500 text-white min-h-screen transition-all duration-1000 ease-out">
+      <div className="m-auto py-20 max-w-md sm:max-w-lg md:max-w-5xl flex flex-col items-center justify-center text-center mb-8 transition-all duration-1000 ease-out">
+        <div className="meals transition-all duration-1000 ease-out m-0">
+          {' '}
+          {cryptoDiv}
+          {' '}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
