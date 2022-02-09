4ebfdb8c849d8e536d4fd4affc9fb5b50777db54
Temitope Ogunleye <topeogunleye1@gmail.com>
2022-02-09 20:22:16 +0100

    Add test files

---
+++ src/__tests__/cryptoComponents.spec.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import renderer from 'react-test-renderer';
+
+//mocks
+import mockCryptos from '../__mocks__/mockCryptos.js';
+
+import configureStore from 'redux-mock-store';
+
+import Home from '../components/pages/Home';
+
+const mockStore = configureStore([]);
+
+describe('My Connected React-Redux Component', () => {
+  let store;
+  let component;
+
+  beforeEach(() => {
+    store = mockStore({
+      cryptos: mockCryptos,
+    });
+
+    component = renderer.create(
+      <Provider store={store}>
+        <mockCryptos/>
+      </Provider>
+    );
+  });
+
+  it('should render with given state from Redux store', () => {
+    expect(component.toJSON()).toMatchSnapshot();
+  });
+
+  it('should dispatch an action on page load', () => {
+    const actions = store.getActions();
+    expect(actions).toEqual([]);
+  });
+});
