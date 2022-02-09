131b964660f316c748b840dcaf0ecd7c0db0ddef
Temitope Ogunleye <topeogunleye1@gmail.com>
2022-02-09 11:29:54 +0100

    Add Details Component

--- src/components/pages/Details.js
+++ src/components/pages/Details.js
@@ -1,37 +1,75 @@
-import React, { useEffect } from 'react';
-import { useParams } from 'react-router';
-import { useSelector, useDispatch } from 'react-redux';
-import { fetchCryptoDetails } from '../../redux/crypto/crypto';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import HistoryChart from '../HistoryChart';
+import CoinData from '../CoinData';
+import coinGecko from '../../redux/apis/coinGecko';
 
 function Details() {
   const { id } = useParams();
-  const crypto = useSelector((state) => state.cryptoDetailsReducer.crypto);
+  const [coinData, setCoinData] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
 
-  const dispatch = useDispatch();
+  const formatData = (data) => data.map((el) => ({
+    t: el[0],
+    y: el[1].toFixed(2),
+  }));
+
   useEffect(() => {
-    dispatch(fetchCryptoDetails(id));
-    console.log(crypto);
+    const fetchData = async () => {
+      setIsLoading(true);
+      const [day, week, year, detail] = await Promise.all([
+        coinGecko.get(`/coins/${id}/market_chart/`, {
+          params: {
+            vs_currency: 'usd',
+            days: '1',
+          },
+        }),
+        coinGecko.get(`/coins/${id}/market_chart/`, {
+          params: {
+            vs_currency: 'usd',
+            days: '7',
+          },
+        }),
+        coinGecko.get(`/coins/${id}/market_chart/`, {
+          params: {
+            vs_currency: 'usd',
+            days: '365',
+          },
+        }),
+        coinGecko.get('/coins/markets/', {
+          params: {
+            vs_currency: 'usd',
+            ids: id,
+          },
+        }),
+      ]);
+      console.log(day);
+
+      setCoinData({
+        day: formatData(day.data.prices),
+        week: formatData(week.data.prices),
+        year: formatData(year.data.prices),
+        detail: detail.data[0],
+      });
+      setIsLoading(false);
+    };
+
+    fetchData();
   }, []);
 
-  const cryptoDetailsDiv = (
-    <div className="flex flex-col items-center justify-center text-center mb-8">
-      <div className="meals transition-all duration-1000 ease-out m-0">
-        <div className="meal hover:shadow-lg transition-all duration-1000 ease-out">
-          <h1>{crypto.name}</h1>
-          <img
-            src={crypto.image}
-            alt={crypto.name}
-            className="h-40 sm:h-40 w-full object-cover hover:opacity-75"
-          />
-          <p>
-            ₦
-            {crypto.current_price}
-          </p>
-        </div>
+  const renderData = () => {
+    if (isLoading) {
+      return <div>Loading....</div>;
+    }
+    return (
+      <div className="coinlist">
+        <HistoryChart data={coinData} />
+        <CoinData data={coinData.detail} />
       </div>
-    </div>
-  );
-  return <div>{cryptoDetailsDiv}</div>;
+    );
+  };
+
+  return renderData();
 }
 
 export default Details;
